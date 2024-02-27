3f37a53bbbd7b31ba19d80ab13ac49d4c3e23eb1
rafigarcia <rafaela.garcia.angulo@gmail.com>
2024-02-26 18:44:35 -0600

    W1 Prepare csharp folder

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade?");
+        string number = Console.ReadLine();
+        int grade = int.Parse(number);
+        
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        int lastDigit = grade % 10;
+        string modifier = "";
+        
+        if (grade >= 90)
+        {
+            modifier ="";
+        }
+    
+        else if (grade >= 60)
+        {
+            if (lastDigit >= 7)
+            {
+                modifier = "+";
+            }
+            else if (lastDigit <= 3 && lastDigit != 0)
+            {
+                modifier = "-";
+            }   
+        }
+        
+        Console.WriteLine($"Your grade is {grade}, and your letter grade is {modifier}{letter}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+
+        else {
+            Console.WriteLine("You failed.");
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,67 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        int input = -1;
+        
+        while (input != 0) 
+        {
+            Console.WriteLine("Enter a number: ");
+            input = int.Parse(Console.ReadLine());
+           
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum of the numbers is: {sum}");
+        float average = ((float)sum) / numbers.Count;
+        
+        Console.WriteLine($"The average of the numbers is: {average}");
+        
+        int max = numbers [0];
+        
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The maximum number is: {max}");
+
+
+        int min = numbers [0];
+
+        foreach (int number in numbers)
+        {
+            if (number < min)
+            {
+                min = number;
+            }
+        }
+        Console.WriteLine($"The minimum number is: {min}");
+
+        numbers.Sort();
+
+        Console.WriteLine($"The sorted numbers are:");
+        foreach (int number in numbers)
+        {
+        Console.WriteLine($"{number} ");
+        }
+
     }
 }
